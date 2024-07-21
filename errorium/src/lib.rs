eeb60f429d4faef7c66d48f430d57f1310ba1407
Mr-Leshiy <leshiy12345678@gmail.com>
2024-07-21 17:59:20 +0300

    add Result type alias

--- errorium/src/lib.rs
+++ errorium/src/lib.rs
@@ -7,3 +7,9 @@
 //! with additional error type safety by providing error "taging".
 
 pub use errorium_macro::*;
+
+/// `Result<T, Box<dyn std::error::Error>>` type alias
+///
+/// As `errorium` based on the `Box<dyn std::error::Error>` generic type, this type alias
+/// will reduce the boilerplate code in your application.
+pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
